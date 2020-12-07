49dc2d29a782afc44bcc2fabd9f6d73e45113512
liga <volodymyr.zubenko@chisw.com>
2020-12-07 12:56:42 +0200

    init

---
+++ src/main/java/com/visoft/helper/service/HelperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.visoft.helper.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelperServiceApplication.class, args);
+    }
+
+}
