ef0a970c27878d1eba40d7853cf2c64c5face3ba
liga <volodymyr.zubenko@chisw.com>
2020-12-22 16:43:52 +0200

    added cors configuration

--- src/main/java/com/visoft/helper/service/configuration/security/SecurityConfig.java
+++ src/main/java/com/visoft/helper/service/configuration/security/SecurityConfig.java
@@ -29,7 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and()
+        http.cors()
+                .and()
+                .httpBasic()
+                .and()
                 .addFilterBefore(
                         new StatelessAuthFilter(tokenAuthService),
                         UsernamePasswordAuthenticationFilter.class
