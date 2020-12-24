83f0c4992025fb20c279e019094fd688853282a9
liga <volodymyr.zubenko@chisw.com>
2020-12-24 10:54:48 +0200

    added to application tree ids

--- src/main/java/com/visoft/helper/service/transport/dto/tree/ApplicationTreeOutcomeDto.java
+++ src/main/java/com/visoft/helper/service/transport/dto/tree/ApplicationTreeOutcomeDto.java
@@ -1,6 +1,6 @@
 package com.visoft.helper.service.transport.dto.tree;
 
-import com.visoft.helper.service.transport.dto.MultiLanguageNameDto;
+import com.visoft.helper.service.transport.dto.MultiLanguageNameIdDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
-public class ApplicationTreeOutcomeDto extends MultiLanguageNameDto {
+public class ApplicationTreeOutcomeDto extends MultiLanguageNameIdDto {
 
     private List<TreeContentDto> content;
 }
