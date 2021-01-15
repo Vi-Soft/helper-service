7b01e15612ca2e0b52176b86a87f234fb430c026
liga <volodymyr.zubenko@chisw.com>
2021-01-15 15:40:46 +0200

    Add requirements to IdDto.java

--- src/main/java/com/visoft/helper/service/transport/dto/IdDto.java
+++ src/main/java/com/visoft/helper/service/transport/dto/IdDto.java
@@ -3,9 +3,12 @@ package com.visoft.helper.service.transport.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class IdDto {
 
+    @NotNull
     private Long id;
 }
