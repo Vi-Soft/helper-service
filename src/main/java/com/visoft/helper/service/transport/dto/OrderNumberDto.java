295142d848a9faa19dbe8e9b5d55261363a363c5
liga <volodymyr.zubenko@chisw.com>
2021-01-15 16:27:16 +0200

    Add requirements to OrderNumberDto

--- src/main/java/com/visoft/helper/service/transport/dto/OrderNumberDto.java
+++ src/main/java/com/visoft/helper/service/transport/dto/OrderNumberDto.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class OrderNumberDto extends MultiLanguageNameDto{
 
     @Min(0)
+    @NotNull
     private int orderNumber;
 }
