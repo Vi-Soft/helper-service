006b29e9e8388a78c5ddea09186e99f5261bf569
liga <volodymyr.zubenko>
2021-01-17 11:06:02 +0200

    added requirements to LoginOutcomeDto

--- src/main/java/com/visoft/helper/service/transport/dto/authorization/LoginOutcomeDto.java
+++ src/main/java/com/visoft/helper/service/transport/dto/authorization/LoginOutcomeDto.java
@@ -3,8 +3,12 @@ package com.visoft.helper.service.transport.dto.authorization;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class LoginOutcomeDto {
+
+    @NotNull
     private String token;
 }
