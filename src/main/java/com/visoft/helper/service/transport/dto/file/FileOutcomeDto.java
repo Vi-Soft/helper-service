1fb2e3e5a6eb6a418ab5b8cc04587f55bba8d766
liga <volodymyr.zubenko>
2021-01-17 11:07:07 +0200

    added requirements to FileOutcomeDto

--- src/main/java/com/visoft/helper/service/transport/dto/file/FileOutcomeDto.java
+++ src/main/java/com/visoft/helper/service/transport/dto/file/FileOutcomeDto.java
@@ -4,12 +4,21 @@ import com.visoft.helper.service.transport.dto.OrderNumberIdDto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class FileOutcomeDto extends OrderNumberIdDto {
 
+    @NotNull
     private String pathEn;
+
+    @NotNull
     private String pathRu;
+
+    @NotNull
     private String pathHe;
+
+    @NotNull
     private Long folderId;
 }
