3e91e0cf756ef99d4ddf63016129839002df0f92
oleksandr-kibkalo <oleksandr.kibkalo@chisw.com>
2021-07-27 14:45:11 +0300

    rns-2352 cors configuration fixed

--- src/main/java/com/visoft/helper/service/configuration/CorsConfiguration.java
+++ src/main/java/com/visoft/helper/service/configuration/CorsConfiguration.java
@@ -1,19 +1,12 @@
 package com.visoft.helper.service.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -25,5 +18,15 @@ public class CorsConfiguration implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
--- src/main/java/com/visoft/helper/service/controller/utils/VersionController.java
+++ src/main/java/com/visoft/helper/service/controller/utils/VersionController.java
@@ -2,6 +2,7 @@ package com.visoft.helper.service.controller.utils;
 
 import com.visoft.helper.service.service.utils.VersionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class VersionController {
 
     private final VersionService versionService;
 
+    @CrossOrigin
     @GetMapping
     public String version(){
         return versionService.getApplicationVersion();
