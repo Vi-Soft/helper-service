b69da3376fcc9fb3a60ca9053cb94d89497e6e0c
zubenko <zubenko>
2022-05-26 13:03:46 +0300

    fix cors

--- src/main/java/com/visoft/helper/service/configuration/CorsConfiguration.java
+++ src/main/java/com/visoft/helper/service/configuration/CorsConfiguration.java
@@ -1,12 +1,61 @@
 package com.visoft.helper.service.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
+
+    /**
+     * + allow all Origins<p>
+     * + allow all Headers<p>
+     * + allow all Methods<p>
+     * + exposed specified headers
+     * + set max age to 30 minutes<p>
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3002")
+                .allowedMethods(allowedMethods())
+                .allowedHeaders(allowedHeaders())
+                .exposedHeaders(exposedHeaders());
+    }
+
+    /**
+     * @return allowed methods as array of strings
+     */
+    private String[] allowedMethods() {
+        return Stream.of(HttpMethod.values())
+                .map(HttpMethod::toString)
+                .toArray(String[]::new);
+    }
+
+    /**
+     * @return special value as asterisk sign specified all headers
+     */
+    private String allowedHeaders() {
+        return "*";
+    }
+
+    /**
+     * @return a string of exposed headers as values delimited by a comma sign
+     */
+    private String exposedHeaders() {
+        return String.join(
+                ", ",
+                new String[]{
+                        "Content-Length",
+                        "Content-Disposition"
+                });
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -18,15 +67,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+        registry
+                .addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
     }
 }
