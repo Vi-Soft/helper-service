83a1dced2eb64b96bcb30e67d72cf2be7f6606e5
antonpryadko <anton.pryadko@chisw.com>
2022-06-01 11:31:13 +0300

    RNS-4279 : added order

--- src/main/java/com/visoft/helper/service/transport/dto/tree/TreeContentDto.java
+++ src/main/java/com/visoft/helper/service/transport/dto/tree/TreeContentDto.java
@@ -1,6 +1,5 @@
 package com.visoft.helper.service.transport.dto.tree;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +13,5 @@ public class TreeContentDto extends ApplicationTreeOutcomeDto {
 
     private String pathHe;
 
-    @JsonIgnore
     private int orderNumber;
 }
