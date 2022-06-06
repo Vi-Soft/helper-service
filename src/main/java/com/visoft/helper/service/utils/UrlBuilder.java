b99a57884016205dc7da9c30123fa0c93aaa097e
antonpryadko <anton.pryadko@chisw.com>
2022-06-06 16:22:46 +0300

    RNS-4279 : changed generate method

--- src/main/java/com/visoft/helper/service/utils/UrlBuilder.java
+++ src/main/java/com/visoft/helper/service/utils/UrlBuilder.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Paths;
+import java.io.File;
 
 @Setter(onMethod_ = @Autowired)
 @Component
@@ -15,11 +15,9 @@ public class UrlBuilder {
 
     public String generate(Language language, String fileNameWithExtension) {
         return
-                Paths.get(
-                        dirStructureCreatorProperties.appResourcesHostAndPort(),
-                        dirStructureCreatorProperties.getApiFolder(),
-                        language.getDescription(),
-                        fileNameWithExtension
-                ).toString();
+                dirStructureCreatorProperties.appResourcesHostAndPort() + File.separator +
+                dirStructureCreatorProperties.getApiFolder() + File.separator +
+                language.getDescription() + File.separator +
+                fileNameWithExtension;
     }
 }
